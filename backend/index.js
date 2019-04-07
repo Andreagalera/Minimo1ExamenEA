966b1e18d2197927c3d2ab3bb5b2ca4ebf616591
Andreagalera <1d09galeraandrea@gmail.com>
2019-04-07 20:42:56 +0200

    first commit

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const cors = require('cors');
+const { mongoose } = require('./database');
+
+//Configuración servidor
+//Puerto
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares: procesar datos
+app.use(morgan('dev'));
+    //servidor entienda formato json
+app.use(express.json());
+app.use(cors({origin: 'http://localhost:4200'}));
+//Routes
+app.use('/api' ,require('./routes/routes'));
+
+//Iniciar servidor
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
